2dba58055b5213475226fd29b6b624656518340e
rayhan889 <raydev1604@gmail.com>
2023-09-18 14:58:27 +0700

    Add provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
@@ -13,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  authModal
+  authModal,
 }: {
   children: React.ReactNode;
   authModal: React.ReactNode;
@@ -27,17 +28,18 @@ export default function RootLayout({
       )}
     >
       <body className="bg-slate-50 min-h-screen pt-12 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
 
-        {authModal}
+          {authModal}
 
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-        {children}
-        </div>
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
 
-        <Toaster />
-        
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
